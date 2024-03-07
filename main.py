07516ccbd79a632b25e31ffd84f8d21922f08712
Adrian <adrian.lozynskyi@gmail.com>
2024-03-07 10:44:16 +0200

    Add images,temp,sky conditions,fix errors

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
+# Add title, text input, slider, selectbox, and subheader
 st.title("Weather Forecast for the Next Days")
-
 place = st.text_input("Place: ")
-days = st.slider("Forecast days", min_value=1, max_value=5,
+days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
-option = st.selectbox("Select data to view:", ("Temperature", "Sky"))
+option = st.selectbox("Select data to view",
+                      ("Temperature", "Sky"))
+st.subheader(f"{option} for the next {days} days in {place}")
 
-st.subheader(f"{option} for the next {days} in {place}")
+if place:
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-
-def get_data(days):
-    dates = ["2022-25-10","2022-27-10","2022-29-10"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
-
-
-d, t = get_data(days)
-figure = px.line(x=d, y=t, labels={"x": "Date","y": "Temperature (C)"})
-
-st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths, width=120)
+    except KeyError:
+        st.write("That place doesn't exist.")
